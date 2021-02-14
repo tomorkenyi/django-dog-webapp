17d1e69777ac243b33ec1998999875c16f11d8d3
Tamás, Tömörkényi <tamas.tomorkenyi@ge.com>
2021-02-14 14:48:48 +0100

    Add details section

--- costs/urls.py
+++ costs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'costs'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:item_id>/', views.detail, name='detail')
 ]
--- costs/views.py
+++ costs/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Item
 
 
 def index(request):
-    return HttpResponse("Hello world.")
+    items = Item.objects.all()
+    return render(request, 'costs/index.html', {'items': items})
+
+
+def detail(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    return render(request, 'costs/detail.html', {'item': item})
